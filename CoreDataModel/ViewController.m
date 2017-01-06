6336692ed02ca044f024d0396bf98e4f6391c58d
100qu <363719365@qq.com>
2017-01-06 17:25:21 +0800

    no message

--- CoreDataModel/ViewController.m
+++ CoreDataModel/ViewController.m
@@ -7,19 +7,52 @@
 //
 
 #import "ViewController.h"
+#import <CoreData/CoreData.h>
 
 @interface ViewController ()
+
+@property (nonatomic,strong) NSDateFormatter *formatter;
+
+@property (nonatomic,assign) NSTimeInterval  touchDownTime;
+@property (nonatomic,assign) NSTimeInterval  touchUpTime;
 
 @end
 
 @implementation ViewController
-
+- (NSDateFormatter *)formatter{
+    if(!_formatter){
+        _formatter = [[NSDateFormatter alloc]init];
+    }
+    return _formatter;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    NSManagedObjectModel
+//    NSManagedObjectContext
+//    NSPersistentStoreCoordinator
+
 }
 
+- (IBAction)ButtonTapDown:(id)sender{
+    
+    _touchDownTime = [NSDate timeIntervalSinceReferenceDate]*1000;
+    NSLog(@"date:%.f",_touchDownTime);
+   __block NSTimeInterval count = 0;
+    [NSTimer scheduledTimerWithTimeInterval:0.001 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        count++;
+        if (count >= 700) {
+            NSLog(@"stop");
+            [timer invalidate];
+        }
+    }];
 
+}
+- (IBAction)ButtonTapUp:(id)sender{
+    [self.formatter setDateFormat:@"SSS"];
+    _touchUpTime = [NSDate timeIntervalSinceReferenceDate]*1000;
+    NSLog(@"date:%.f",_touchUpTime);
+    NSLog(@"_touchUpTime-_touchDownTime:%.f",_touchUpTime-_touchDownTime);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
